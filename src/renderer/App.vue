8a5e04dd6ae30ccc5b9d0ef2b452fef4ed1b935b
Kazuki Tobita <kazukiti201@gmail.com>
2018-02-18 02:33:14 +0900

    いらなさそうなファイルを消してみた

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,15 +1,5 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+    <div>
+        hoge
+    </div>
 </template>
-
-<script>
-  export default {
-    name: 'markury'
-  }
-</script>
-
-<style>
-  /* CSS */
-</style>
