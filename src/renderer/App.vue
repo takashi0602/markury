32329e6c34c4a9ceffea8e529a7d2809ee54bf6d
Kazuki Tobita <kazukiti201@gmail.com>
2018-02-18 02:36:22 +0900

    デザイン周り

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,5 +1,56 @@
-<template>
-    <div>
-        hoge
-    </div>
+<template lang="pug">
+    .container
+        textarea.markdown
+        .result
+            h1 hoge
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                input: '# hello, markury!'
+            }
+        },
+
+    }
+</script>
+
+<style lang="scss">
+    html, body, #app {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+    .container {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+    }
+    .markdown {
+        display: inline-block;
+        width: 50%;
+        height: 100%;
+        vertical-align: top;
+        box-sizing: border-box;
+        padding: 20px;
+        border: none;
+        border-right: 1px solid #ccc;
+        resize: none;
+        outline: none;
+        background-color: #f6f6f6;
+        font-size: 14px;
+        font-family: 'Monaco', courier, monospace;
+    }
+    .result {
+        display: inline-block;
+        width: 50%;
+        height: 100%;
+        vertical-align: top;
+        box-sizing: border-box;
+        padding: 0 20px;
+    }
+</style>
